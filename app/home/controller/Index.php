7c6fd13537c042d021dd7ba1889bc67bc82c65af
shuta <648949076@qq.com>
2020-09-02 20:48:25 +0800

    PC手机分离，手机端页面优化

--- app/home/controller/Index.php
+++ app/home/controller/Index.php
@@ -17,13 +17,61 @@ use app\BaseController;
 use think\facade\View;
 use app\model\BlogArticle;
 class Index 
-{
+{   
+
+    private function pageType($vi)
+    {
+        if( !$this->ismobile() ){
+            return View::fetch($vi);
+        }else{
+            return View::fetch('/mobile/'.$vi);
+        }
+    }
+
+
+
+    private function ismobile()
+    {
+        // 如果有HTTP_X_WAP_PROFILE则一定是移动设备
+        if (isset ($_SERVER['HTTP_X_WAP_PROFILE']))
+            return true;
+
+        //此条摘自TPM智能切换模板引擎，适合TPM开发
+        if (isset ($_SERVER['HTTP_CLIENT']) && 'PhoneClient' == $_SERVER['HTTP_CLIENT'])
+            return true;
+        //如果via信息含有wap则一定是移动设备,部分服务商会屏蔽该信息
+        if (isset ($_SERVER['HTTP_VIA']))
+            //找不到为flase,否则为true
+            return stristr($_SERVER['HTTP_VIA'], 'wap') ? true : false;
+        //判断手机发送的客户端标志,兼容性有待提高
+        if (isset ($_SERVER['HTTP_USER_AGENT'])) {
+            $clientkeywords = array(
+                'nokia', 'sony', 'ericsson', 'mot', 'samsung', 'htc', 'sgh', 'lg', 'sharp', 'sie-', 'philips', 'panasonic', 'alcatel', 'lenovo', 'iphone', 'ipod', 'blackberry', 'meizu', 'android', 'netfront', 'symbian', 'ucweb', 'windowsce', 'palm', 'operamini', 'operamobi', 'openwave', 'nexusone', 'cldc', 'midp', 'wap', 'mobile'
+            );
+            //从HTTP_USER_AGENT中查找手机浏览器的关键字
+            if (preg_match("/(" . implode('|', $clientkeywords) . ")/i", strtolower($_SERVER['HTTP_USER_AGENT']))) {
+                return true;
+            }
+        }
+        //协议法，因为有可能不准确，放到最后判断
+        if (isset ($_SERVER['HTTP_ACCEPT'])) {
+            // 如果只支持wml并且不支持html那一定是移动设备
+            // 如果支持wml和html但是wml在html之前则是移动设备
+            if ((strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') !== false) && (strpos($_SERVER['HTTP_ACCEPT'], 'text/html') === false || (strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') < strpos($_SERVER['HTTP_ACCEPT'], 'text/html')))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
     public function index()
     {	
     	$list = BlogArticle::order('update_time desc')->select();
     	// dump($list->toArray());
     	View::assign('list',$list);
-        return View::fetch();
+        // return View::fetch('index/index');
+        return $this->pageType('index/index');
     }
 
     /**
@@ -38,11 +86,12 @@ class Index
     	$html = $parser->makeHtml($detail->toArray()['content']);
     	View::assign('detail',$detail);
     	View::assign('html',$html);
-    	return View::fetch();
+    	return $this->pageType('index/detail');
     }
 
     public function hello($name = 'ThinkPHP6')
-    {
+    {   
+        
         return 'hello,' . $name;
     }
 }
